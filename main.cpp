9bcfd378be07cd4d961ab1e7eac9d179c5d59d5b
clarence00516 <clarence00516@gmail.com>
2025-10-05 18:04:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//Irishmae L. Petallar
+//BSESM 1B
+//JANUARY 11, 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2,num3,num4;
+    int p;
+    cout << "ENTER FIRST NUMBER: ";
+    cin >> num1;
+    cout << "ENTER SECOND NUMBER: ";
+    cin >> num2;
+    cout << "ENTER THIRD NUMBER: ";
+    cin >> num3;
+    cout << "ENTER FOURTH NUMBER: ";
+    cin >> num4;
+    p=num1*num2*num3*num4;
+    cout << "PRODUCT IS EQUALS " << p;
+
+
+    return 0;
+}
